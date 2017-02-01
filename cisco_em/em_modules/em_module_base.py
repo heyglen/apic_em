9e9b4b1a91b712a49ab21e4e82cb87c1a001fe20
Glen Harmon <glencharmon@gmail.com>
2017-02-01 13:11:14 +0100

    refactor

---
+++ cisco_em/em_modules/em_module_base.py
@@ -0,0 +1,32 @@
+
+
+from collections import namedtuple
+
+
+class EmModuleBase(object):
+    def __init__(self, cisco_em):
+        self._cisco_em = cisco_em
+        self._headers = cisco_em._headers
+        self._url = cisco_em._url
+
+    def _build(self, object_name, object_dict, display=None):
+        display = display or object_name
+
+        class ManagedObject(namedtuple(object_name, object_dict.keys())):
+            _display = None
+
+            def __str__(self):
+                if not hasattr(self, self._display):
+                    self._display = str(vars(self))
+                else:
+                    self._display = getattr(self, self._display)
+                return str(self._display)
+
+            def __repr__(self):
+                return '<{} {}>'.format(
+                    object_name,
+                    self.__str__()
+                )
+        ManagedObject._display = display
+        item = ManagedObject(**object_dict)
+        return item
